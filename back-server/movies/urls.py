ef0c00966826d3671992631540431d0d38b69809
hyejin0705 <hyejin3681@gmail.com>
2022-11-18 01:30:47 +0900

    like, review 기능 구현,  server8081 add

--- back-server/movies/urls.py
+++ back-server/movies/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('create/', views.movie_create),
     path('<int:movie_id>/', views.movie_detail),
 
-    path('<int:movie_id>/like', views.like)
+    path('<int:movie_id>/like', views.like),
+
+    path('<int:movie_id>/reviews/', views.review_create),
+
+    # path('reviews/', views.review_list),
+    path('reviews/<int:review_pk>', views.review_detail),
 ]
