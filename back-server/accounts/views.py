1fef33233544fb83be3ac8c409106d4c59b63798
hyejin0705 <hyejin3681@gmail.com>
2022-11-21 02:39:47 +0900

    update like

--- back-server/accounts/views.py
+++ back-server/accounts/views.py
@@ -6,9 +6,19 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
 # Create your views here.
+
 @api_view(['DELETE'])
 def user_delete(request):
     request.delete()     # 탈퇴 후 로그아웃
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
+def profile(request, username):
+    pass
+
+
+@api_view(['GET'])
+def follow(request, user_pk):
+    pass
